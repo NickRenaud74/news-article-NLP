dc03c87ef5517b13f9cfcb59f0766d017adb2d2b
Nick Renaud <nick_renaud@hotmail.com>
2020-09-08 15:27:39 -0700

    Setting up project configuration

---
+++ webpack.dev.js
@@ -0,0 +1,21 @@
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        })
+    ]
+}
