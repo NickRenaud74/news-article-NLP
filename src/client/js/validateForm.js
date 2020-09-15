738677f2099c4dae3bd6a6e9e2963285f575e853
Nick Renaud <nick_renaud@hotmail.com>
2020-09-15 09:33:34 -0700

    update form validation

--- src/client/js/validateForm.js
+++ src/client/js/validateForm.js
@@ -1,7 +1,7 @@
 const validateForm = () => {
     let txt = document.getElementById('text').value;
     if (txt.length === 0) {
-        alert('Please enter a sentence into the form')
+        alert('Please enter some text into the form')
     };
 };
 
